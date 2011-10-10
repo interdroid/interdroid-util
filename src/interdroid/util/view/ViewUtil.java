b306bc100d2b1e20b43ef9f8fe57b843fe415761
Nick Palmer <nick@sluggardy.net>
2011-10-10 18:40:52 +0200

    Make a new ViewUtil class with some utiltiy methods for working with views.

---
+++ src/interdroid/util/view/ViewUtil.java
@@ -0,0 +1,42 @@
+package interdroid.util.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Various static utility methods for working with views.
+ *
+ * @author nick &lt;palmer@cs.vu.nl&gt;
+ *
+ */
+public class ViewUtil {
+	/**
+	 * Returns the layout inflater for a given activity.
+	 * @param activity The activity to get the inflater from.
+	 * @return The layout inflater used by this activity
+	 */
+	public static LayoutInflater getLayoutInflater(final Activity activity) {
+		return (LayoutInflater) activity.getApplicationContext()
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+	}
+
+	/**
+	 * Adds a view on the UI thread of the given activity.
+	 * @param activity The activity to run on the UI thread of
+	 * @param viewGroup The view group to add to
+	 * @param view The view to be added
+	 */
+	public static void addView(final Activity activity,
+			final ViewGroup viewGroup, final View view) {
+		if (viewGroup != null) {
+			activity.runOnUiThread(new Runnable() {
+				public void run() {
+					viewGroup.addView(view);
+				}
+			});
+		}
+	}
+}
