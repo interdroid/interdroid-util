995967c1c8dc3594797ee97cdfb4b9cb90bb31db
Nick Palmer <nick@sluggardy.net>
2012-04-23 21:30:18 +0200

    Licensing and whitespace cleanup.

--- src/interdroid/util/DbUtil.java
+++ src/interdroid/util/DbUtil.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2009-2012 Vrije Universiteit, The Netherlands All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Vrije Universiteit nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package interdroid.util;
 
 import java.util.Map.Entry;
@@ -9,103 +39,103 @@ import android.content.ContentValues;
 import android.database.Cursor;
 
 public final class DbUtil {
-    /**
-     * Access to Logger.
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(DbUtil.class);
+	/**
+	 * Access to Logger.
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(DbUtil.class);
 
-    /**
-     * No construction.
-     */
-    private DbUtil() { }
+	/**
+	 * No construction.
+	 */
+	private DbUtil() { }
 
-    /**
-     * Returns a quoted column name if the column is not already quoted.
-     * @param columnName the name of the column to quote
-     * @return the quoted column name
-     */
-    public static String quoteColumnName(final String columnName) {
-        if (columnName.charAt(0) != '\"') {
-            return "\"" + columnName + "\"";
-        }
-        return columnName;
-    }
+	/**
+	 * Returns a quoted column name if the column is not already quoted.
+	 * @param columnName the name of the column to quote
+	 * @return the quoted column name
+	 */
+	public static String quoteColumnName(final String columnName) {
+		if (columnName.charAt(0) != '\"') {
+			return "\"" + columnName + "\"";
+		}
+		return columnName;
+	}
 
-    /**
-     * Returns an array of quoted column names
-     * @param columnNames the names of the columns to quote.
-     * @return an array of quoted column names
-     */
-    public static String[] quoteColumnNames(final String[] columnNames) {
-        String[] ret = null;
-        if (columnNames != null) {
-            ret = new String[columnNames.length];
-            for (int i = 0; i < columnNames.length; i++) {
-                ret[i] = quoteColumnName(columnNames[i]);
-            }
-        }
-        return ret;
-    }
+	/**
+	 * Returns an array of quoted column names
+	 * @param columnNames the names of the columns to quote.
+	 * @return an array of quoted column names
+	 */
+	public static String[] quoteColumnNames(final String[] columnNames) {
+		String[] ret = null;
+		if (columnNames != null) {
+			ret = new String[columnNames.length];
+			for (int i = 0; i < columnNames.length; i++) {
+				ret[i] = quoteColumnName(columnNames[i]);
+			}
+		}
+		return ret;
+	}
 
-    /**
-     * Returns the index for a field.
-     * @param c the cursor to check in
-     * @param columnName the column name to look for
-     * @return the index, or -1
-     */
-    public static int getFieldIndex(Cursor c, String columnName) {
-        int index = c.getColumnIndex(columnName);
-        if (index == -1) {
-            index = c.getColumnIndex(quoteColumnName(columnName));
-        }
-        if (index == -1) {
-            LOG.warn("Unable to find column index: {} in {}",
-                    columnName, c.getColumnNames());
-        }
-        return index;
-    }
+	/**
+	 * Returns the index for a field.
+	 * @param c the cursor to check in
+	 * @param columnName the column name to look for
+	 * @return the index, or -1
+	 */
+	public static int getFieldIndex(Cursor c, String columnName) {
+		int index = c.getColumnIndex(columnName);
+		if (index == -1) {
+			index = c.getColumnIndex(quoteColumnName(columnName));
+		}
+		if (index == -1) {
+			LOG.warn("Unable to find column index: {} in {}",
+					columnName, c.getColumnNames());
+		}
+		return index;
+	}
 
-    /**
-     * Android does not quote identifiers so we have to handle that.
-     * This returns a new ContentValues where the columns have all been
-     * quoted properly.
-     *
-     * @param values the values to be quoted
-     * @return a sanitized version of the values with quoted column names
-     */
-    public static ContentValues quoteColumnNames(final ContentValues values) {
-        ContentValues cleanValues = new ContentValues();
-        for (Entry<String, Object> val : values.valueSet()) {
-            Object value = val.getValue();
-            String cleanName = val.getKey();
-            cleanName = quoteColumnName(cleanName);
-            // This really sucks. There is no generic put an object....
-            if (value == null) {
-                cleanValues.putNull(cleanName);
-            } else if (value instanceof Boolean) {
-                cleanValues.put(cleanName, (Boolean) value);
-            } else if (value instanceof Byte) {
-                cleanValues.put(cleanName, (Byte) value);
-            } else if (value instanceof byte[]) {
-                cleanValues.put(cleanName, (byte[]) value);
-            } else if (value instanceof Double) {
-                cleanValues.put(cleanName, (Double) value);
-            } else if (value instanceof Float) {
-                cleanValues.put(cleanName, (Float) value);
-            } else if (value instanceof Integer) {
-                cleanValues.put(cleanName, (Integer) value);
-            } else if (value instanceof Long) {
-                cleanValues.put(cleanName, (Long) value);
-            } else if (value instanceof Short) {
-                cleanValues.put(cleanName, (Short) value);
-            } else if (value instanceof String) {
-                cleanValues.put(cleanName, (String) value);
-            } else {
-                throw new RuntimeException(
-                        "Don't know how to add value of type: "
-                                + value.getClass().getCanonicalName());
-            }
-        }
-        return cleanValues;
-    }
+	/**
+	 * Android does not quote identifiers so we have to handle that.
+	 * This returns a new ContentValues where the columns have all been
+	 * quoted properly.
+	 *
+	 * @param values the values to be quoted
+	 * @return a sanitized version of the values with quoted column names
+	 */
+	public static ContentValues quoteColumnNames(final ContentValues values) {
+		ContentValues cleanValues = new ContentValues();
+		for (Entry<String, Object> val : values.valueSet()) {
+			Object value = val.getValue();
+			String cleanName = val.getKey();
+			cleanName = quoteColumnName(cleanName);
+			// This really sucks. There is no generic put an object....
+			if (value == null) {
+				cleanValues.putNull(cleanName);
+			} else if (value instanceof Boolean) {
+				cleanValues.put(cleanName, (Boolean) value);
+			} else if (value instanceof Byte) {
+				cleanValues.put(cleanName, (Byte) value);
+			} else if (value instanceof byte[]) {
+				cleanValues.put(cleanName, (byte[]) value);
+			} else if (value instanceof Double) {
+				cleanValues.put(cleanName, (Double) value);
+			} else if (value instanceof Float) {
+				cleanValues.put(cleanName, (Float) value);
+			} else if (value instanceof Integer) {
+				cleanValues.put(cleanName, (Integer) value);
+			} else if (value instanceof Long) {
+				cleanValues.put(cleanName, (Long) value);
+			} else if (value instanceof Short) {
+				cleanValues.put(cleanName, (Short) value);
+			} else if (value instanceof String) {
+				cleanValues.put(cleanName, (String) value);
+			} else {
+				throw new RuntimeException(
+						"Don't know how to add value of type: "
+								+ value.getClass().getCanonicalName());
+			}
+		}
+		return cleanValues;
+	}
 }
--- src/interdroid/util/StrictUtil.java
+++ src/interdroid/util/StrictUtil.java
@@ -1,3 +1,33 @@
+/*
+ * Copyright (c) 2008-2012 Vrije Universiteit, The Netherlands All rights
+ * reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Vrije Universiteit nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
 package interdroid.util;
 
 import java.lang.reflect.Constructor;
@@ -17,167 +47,167 @@ import org.slf4j.LoggerFactory;
  *
  */
 public final class StrictUtil {
-    /**
-     * Access to logger.
-     */
-    private static final Logger LOG = LoggerFactory.getLogger(StrictUtil.class);
+	/**
+	 * Access to logger.
+	 */
+	private static final Logger LOG = LoggerFactory.getLogger(StrictUtil.class);
 
-    /**
-     * No construction.
-     */
-    private StrictUtil() {
-        // No construction
-    }
+	/**
+	 * No construction.
+	 */
+	private StrictUtil() {
+		// No construction
+	}
 
-    /**
-     * Is developer mode on.
-     */
-    private static boolean sDeveloperMode = false;
+	/**
+	 * Is developer mode on.
+	 */
+	private static boolean sDeveloperMode = false;
 
-    /**
-     * Turns on developer mode system wide.
-     */
-    public static void setDeveloperMode() {
-        sDeveloperMode = true;
-    }
+	/**
+	 * Turns on developer mode system wide.
+	 */
+	public static void setDeveloperMode() {
+		sDeveloperMode = true;
+	}
 
-    /**
-     * Sets the thread policy for strict mode to all with
-     * log and death penalties.
-     */
-    public static void setThreadPolicies() {
-        if (sDeveloperMode) {
-            Class<?> strictMode;
-            try {
-                strictMode =
-                        Class.forName(
-                                "android.os.StrictMode");
+	/**
+	 * Sets the thread policy for strict mode to all with
+	 * log and death penalties.
+	 */
+	public static void setThreadPolicies() {
+		if (sDeveloperMode) {
+			Class<?> strictMode;
+			try {
+				strictMode =
+						Class.forName(
+								"android.os.StrictMode");
 
-                setThreadPolicy(strictMode);
-            } catch (Exception e) {
-                if (LOG.isWarnEnabled()) {
-                    LOG.warn("Exception setting Strict ThreadPolicy", e);
-                }
-            }
-        }
-    }
+				setThreadPolicy(strictMode);
+			} catch (Exception e) {
+				if (LOG.isWarnEnabled()) {
+					LOG.warn("Exception setting Strict ThreadPolicy", e);
+				}
+			}
+		}
+	}
 
-    /**
-     * Sets the thread and vm policy for strict mode to all with log
-     * and death penalties.
-     */
-    public static void setDeveloperPolicies() {
-        if (sDeveloperMode) {
-            Class<?> strictMode;
-            try {
-                strictMode =
-                        Class.forName(
-                                "android.os.StrictMode");
+	/**
+	 * Sets the thread and vm policy for strict mode to all with log
+	 * and death penalties.
+	 */
+	public static void setDeveloperPolicies() {
+		if (sDeveloperMode) {
+			Class<?> strictMode;
+			try {
+				strictMode =
+						Class.forName(
+								"android.os.StrictMode");
 
-                setThreadPolicy(strictMode);
-                setVmPolicy(strictMode);
-            } catch (Exception e) {
-                if (LOG.isWarnEnabled()) {
-                    LOG.warn("Exception setting thread and vm policy", e);
-                }
-            }
-        }
-    }
+				setThreadPolicy(strictMode);
+				setVmPolicy(strictMode);
+			} catch (Exception e) {
+				if (LOG.isWarnEnabled()) {
+					LOG.warn("Exception setting thread and vm policy", e);
+				}
+			}
+		}
+	}
 
-    /**
-     * Sets the vm policy.
-     *
-     * @param strictMode
-     *            the strict mode to work with
-     * @throws ClassNotFoundException
-     *             on problem
-     * @throws NoSuchMethodException
-     *             on problem
-     * @throws InstantiationException
-     *             on problem
-     * @throws IllegalAccessException
-     *             on problem
-     * @throws InvocationTargetException
-     *             on problem
-     */
-    private static void setVmPolicy(final Class<?> strictMode)
-            throws ClassNotFoundException, NoSuchMethodException,
-            InstantiationException, IllegalAccessException,
-            InvocationTargetException {
-        Class<?> vmPolicyType =
-                Class.forName("android.os.StrictMode$VmPolicy");
-        Method setVmPolicy =
-                strictMode.getMethod("setVmPolicy",
-                        vmPolicyType);
-        Class<?> vmPolicyBuilder = Class
-                .forName(
-                    "android.os.StrictMode$VmPolicy$Builder");
+	/**
+	 * Sets the vm policy.
+	 *
+	 * @param strictMode
+	 *            the strict mode to work with
+	 * @throws ClassNotFoundException
+	 *             on problem
+	 * @throws NoSuchMethodException
+	 *             on problem
+	 * @throws InstantiationException
+	 *             on problem
+	 * @throws IllegalAccessException
+	 *             on problem
+	 * @throws InvocationTargetException
+	 *             on problem
+	 */
+	private static void setVmPolicy(final Class<?> strictMode)
+			throws ClassNotFoundException, NoSuchMethodException,
+			InstantiationException, IllegalAccessException,
+			InvocationTargetException {
+		Class<?> vmPolicyType =
+				Class.forName("android.os.StrictMode$VmPolicy");
+		Method setVmPolicy =
+				strictMode.getMethod("setVmPolicy",
+						vmPolicyType);
+		Class<?> vmPolicyBuilder = Class
+				.forName(
+					"android.os.StrictMode$VmPolicy$Builder");
 
-        Object policy = buildPolicy(vmPolicyBuilder);
-        setVmPolicy.invoke(strictMode, policy);
-    }
+		Object policy = buildPolicy(vmPolicyBuilder);
+		setVmPolicy.invoke(strictMode, policy);
+	}
 
-    /**
-     * Sets the thread policy.
-     *
-     * @param strictMode
-     *            the strict mode to work with
-     * @throws ClassNotFoundException
-     *             on problem
-     * @throws NoSuchMethodException
-     *             on problem
-     * @throws InstantiationException
-     *             on problem
-     * @throws IllegalAccessException
-     *             on problem
-     * @throws InvocationTargetException
-     *             on problem
-     */
-    private static void setThreadPolicy(final Class<?> strictMode)
-            throws ClassNotFoundException, NoSuchMethodException,
-            InstantiationException, IllegalAccessException,
-            InvocationTargetException {
+	/**
+	 * Sets the thread policy.
+	 *
+	 * @param strictMode
+	 *            the strict mode to work with
+	 * @throws ClassNotFoundException
+	 *             on problem
+	 * @throws NoSuchMethodException
+	 *             on problem
+	 * @throws InstantiationException
+	 *             on problem
+	 * @throws IllegalAccessException
+	 *             on problem
+	 * @throws InvocationTargetException
+	 *             on problem
+	 */
+	private static void setThreadPolicy(final Class<?> strictMode)
+			throws ClassNotFoundException, NoSuchMethodException,
+			InstantiationException, IllegalAccessException,
+			InvocationTargetException {
 
-        Class<?> threadPolicyType = Class
-                .forName("android.os.StrictMode$ThreadPolicy");
-        Method setThreadPolicy = strictMode.getMethod("setThreadPolicy",
-                threadPolicyType);
-        Class<?> threadPolicyBuilder = Class
-                .forName(
-                    "android.os.StrictMode$ThreadPolicy$Builder");
+		Class<?> threadPolicyType = Class
+				.forName("android.os.StrictMode$ThreadPolicy");
+		Method setThreadPolicy = strictMode.getMethod("setThreadPolicy",
+				threadPolicyType);
+		Class<?> threadPolicyBuilder = Class
+				.forName(
+					"android.os.StrictMode$ThreadPolicy$Builder");
 
-        Object policy = buildPolicy(threadPolicyBuilder);
-        setThreadPolicy.invoke(strictMode, policy);
-    }
+		Object policy = buildPolicy(threadPolicyBuilder);
+		setThreadPolicy.invoke(strictMode, policy);
+	}
 
-    /**
-     * Builds a policy with detectAll, penaltyLog and penaltyDeath.
-     *
-     * @param policyBuilder
-     *            the builder to use
-     * @return the policy object
-     * @throws NoSuchMethodException
-     *             on problem
-     * @throws InstantiationException
-     *             on problem
-     * @throws IllegalAccessException
-     *             on problem
-     * @throws InvocationTargetException
-     *             on problem
-     */
-    private static Object buildPolicy(final Class<?> policyBuilder)
-            throws NoSuchMethodException, InstantiationException,
-            IllegalAccessException, InvocationTargetException {
-        Method detectAll = policyBuilder.getMethod("detectAll");
-        Method penaltyLog = policyBuilder.getMethod("penaltyLog");
-        Method penaltyDeath = policyBuilder.getMethod("penaltyDeath");
-        Method build = policyBuilder.getMethod("build");
-        Constructor<?> constructor = policyBuilder.getConstructor();
+	/**
+	 * Builds a policy with detectAll, penaltyLog and penaltyDeath.
+	 *
+	 * @param policyBuilder
+	 *            the builder to use
+	 * @return the policy object
+	 * @throws NoSuchMethodException
+	 *             on problem
+	 * @throws InstantiationException
+	 *             on problem
+	 * @throws IllegalAccessException
+	 *             on problem
+	 * @throws InvocationTargetException
+	 *             on problem
+	 */
+	private static Object buildPolicy(final Class<?> policyBuilder)
+			throws NoSuchMethodException, InstantiationException,
+			IllegalAccessException, InvocationTargetException {
+		Method detectAll = policyBuilder.getMethod("detectAll");
+		Method penaltyLog = policyBuilder.getMethod("penaltyLog");
+		Method penaltyDeath = policyBuilder.getMethod("penaltyDeath");
+		Method build = policyBuilder.getMethod("build");
+		Constructor<?> constructor = policyBuilder.getConstructor();
 
-        Object builder = constructor.newInstance();
-        builder = detectAll.invoke(builder);
-        builder = penaltyLog.invoke(builder);
-        builder = penaltyDeath.invoke(builder);
-        return build.invoke(builder);
-    }
+		Object builder = constructor.newInstance();
+		builder = detectAll.invoke(builder);
+		builder = penaltyLog.invoke(builder);
+		builder = penaltyDeath.invoke(builder);
+		return build.invoke(builder);
+	}
 }
